00ec9f18cf06a65e2486fe2a6b1217791818b41c
mahmuddevs <63976984+mahmuddevs@users.noreply.github.com>
2022-08-11 08:01:15 +0600

    Add files via upload

---
+++ cf.py
@@ -0,0 +1,33 @@
+#watermelon
+
+    w = int(input())
+     
+    if w <= 2 or w % 2 != 0:
+        print("no")
+    else:
+        print("yes")
+
+#way to long words
+
+
+    c = int(input())
+    for i in range(c):
+        w = input()
+        if len(w) > 10:
+            print(w[0] + str(len(w)-2) + w[-1])
+        else:
+            print(w)
+
+#team
+    ttlable = []
+     
+    e = int(input())
+     
+    for i in range(e):
+        able = input().split()
+        n = 0
+     
+        if able.count("1") >= 2:
+            n += 1
+            ttlable.append(n)
+    print(sum(ttlable))
