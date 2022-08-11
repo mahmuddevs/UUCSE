b57e8299fc3b74cdf352ab9a6ab4573b68e13548
mahmuddevs <63976984+mahmuddevs@users.noreply.github.com>
2022-08-11 08:50:07 +0600

    Update bin2dec.py

--- bin2dec.py
+++ bin2dec.py
@@ -1,13 +1,13 @@
-bin = int(input("Enter A Binary Value: "))
+num = int(input("Enter A Binary Value: "))
 
 dec = 0
 
 power = 0
 
-while bin != 0:
-    rem = bin % 10
+while num != 0:
+    rem = num % 10
     dec = dec + rem * (2 ** power) #(if we use 8 instead of 2 here it will convert octal to decimal)
-    bin = bin // 10
+    num = num // 10
     power += 1
 
 print(dec)
