d4ca42710455b8fb3c3f18dbe30cf09814bd4564
mahmuddevs <63976984+mahmuddevs@users.noreply.github.com>
2022-08-11 08:48:57 +0600

    Update bin2dec.py

--- bin2dec.py
+++ bin2dec.py
@@ -6,7 +6,7 @@ power = 0
 
 while bin != 0:
     rem = bin % 10
-    dec = dec + rem * (2 ** power)
+    dec = dec + rem * (2 ** power) #(if we use 8 instead of 2 here it will convert octal to decimal)
     bin = bin // 10
     power += 1
 
