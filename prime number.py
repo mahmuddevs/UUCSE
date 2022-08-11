8b7c2528e8a02d47c7896184b3ee18a2bd1c4b49
mahmuddevs <63976984+mahmuddevs@users.noreply.github.com>
2022-08-11 08:51:47 +0600

    Update prime number.py

--- prime number.py
+++ prime number.py
@@ -1,18 +1,18 @@
 
 num = int(input("Enter a number: "))
 
-prime = False
+not_prime = False
 
 if num > 1:
     # check for factors
     for i in range(2, num):
         if (num % i) == 0:
-            prime = True
+            not_prime = True
 
             break
 
 
-if prime:
+if not_prime:
     print(num, "is not a prime number")
 else:
     print(num, "is a prime number")
