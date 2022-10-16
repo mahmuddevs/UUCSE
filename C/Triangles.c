34dd260a623d0389d1289d40764455945a8af6e6
mahmuddevs <63976984+mahmuddevs@users.noreply.github.com>
2022-10-16 23:24:45 +0600

    Update Triangles.c

--- C/Triangles.c
+++ C/Triangles.c
@@ -113,6 +113,35 @@ int main()
     }
 
     */
+    
+    /* Diamond */
+     int i, j, r;
+
+    printf("Enter number of rows: ");
+    scanf("%d", &r);
+
+    for (i = 1; i <= r; i++)
+    {
+        for (j = 1; j <= r-i; j++)
+            printf("  ");
+
+        for (j = 1; j <= 2*i-1; j++)
+            printf("* ");
+
+        printf("\n");
+    }
+
+    for (i = 1; i <= r - 1; i++)
+    {
+        for (j = 1; j <= i; j++)
+            printf("  ");
+
+        for (j = 1 ; j <= 2*(r-i)-1; j++)
+            printf("* ");
+
+        printf("\n");
+    }
+
 
 
 
